b5a8fd166b2c4a7cb2f45adcb2631b01d3f45717
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-27 16:36:12 +0200

    add Layout

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,7 +3,6 @@ mod controllers;
 
 use components::*;
 use dioxus::prelude::*;
-use model::{PostShopItem, ShoppingListItem};
 
 const _STYLE: &str = manganis::mg!(file("public/tailwind.css"));
 
@@ -11,19 +10,78 @@ fn main() {
     launch(App);
 }
 
+#[derive(Routable, Clone)]
+enum Route {
+    #[layout(Layout)]
+    #[route("/")]
+    Home {},
+    #[route("/profile")]
+    Profile {},
+}
+
 #[allow(non_snake_case)]
-pub fn App() -> Element {
-    let change_signal = use_signal(|| ListChanged);
-    let rust_basel = "Rust Basel";
+fn App() -> Element {
     rsx! {
-        h1{
-            "Welcome to {rust_basel}"
+        Router::<Route>{}
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn Profile() -> Element {
+    rsx! {
+        div {
+            div {
+                class: "flex flex-col gap-4 w-full",
+                div {
+                    class: "flex gap-4 items-center",
+                    div {
+                        class: "skeleton w-16 h-16 rounded-full shrink-0"
+                    }
+                    div {
+                        class: "flex flex-col hap-4",
+                        div {
+                            class: "skeleton h-4 w-20"
+                        }
+                        div {
+                            class: "skeleton h-4 w-28"
+                        }
+                    }
+                }
+                div {
+                    class: "skeleton h-32 w-full"
+                }
+                div {
+                    class: "skeleton h-32 w-full"
+                }
+            }
         }
-        button{
-            class: "btn",
-            "My stylish button"
-        }
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn Home() -> Element {
+    let change_signal = use_signal(|| ListChanged);
+    rsx! {
         ShoppingList{change_signal}
         ItemInput{change_signal}
+    }
+}
+
+#[component]
+pub fn Layout() -> Element {
+    rsx! {
+        div {
+            class: "min-h-screen bg-base-300",
+            div {
+                class: "navbar flex",
+                div {
+                    Link { class: "p-4", to: Route::Home{}, "Home" }
+                    Link { class: "p-4", to: Route::Profile{}, "Profile" }
+                }
+            }
+            div { class: "container mx-auto max-w-[1024px] p-8",
+                Outlet::<Route>{}
+            }
+        }
     }
 }
