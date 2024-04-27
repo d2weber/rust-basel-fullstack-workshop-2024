e3c92fbc726fd07640210d27723841fa1aa307f5
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-27 20:56:27 +0200

    multiple lists

--- backend/src/controllers.rs
+++ backend/src/controllers.rs
@@ -9,17 +9,21 @@ use uuid::Uuid;
 
 type Database = Arc<RwLock<InMemoryDatabase>>;
 
-pub async fn get_items(State(state): State<Database>) -> impl IntoResponse {
+pub async fn get_items(
+    Path(list_uuid): Path<Uuid>,
+    State(state): State<Database>,
+) -> impl IntoResponse {
     let items: Vec<ShoppingListItem> = state
         .read()
         .unwrap()
-        .iter()
-        .map(|(uuid, item)| item.to_model(uuid))
-        .collect();
+        .iter_list(list_uuid.to_string())
+        .map(|list| list.map(|(uuid, item)| item.to_model(uuid)).collect())
+        .unwrap_or_default();
     Json(items)
 }
 
 pub async fn add_item(
+    Path(list_uuid): Path<Uuid>,
     State(state): State<Database>,
     Json(post_request): Json<PostShopItem>,
 ) -> impl IntoResponse {
@@ -29,7 +33,7 @@ pub async fn add_item(
 
     let uuid = Uuid::new_v4().to_string();
     let item: ShoppingItem = post_request.into();
-    db.insert_item(&uuid, item.clone());
+    db.insert_item(list_uuid.to_string(), &uuid, item.clone());
     (
         StatusCode::OK,
         Json(ShoppingListItem {
@@ -43,13 +47,13 @@ pub async fn add_item(
 
 pub async fn delete_item(
     State(state): State<Database>,
-    Path(uuid): Path<Uuid>,
+    Path((list_uuid, item_uuid)): Path<(Uuid, Uuid)>,
 ) -> impl IntoResponse {
     let Ok(mut db) = state.write() else {
         return StatusCode::SERVICE_UNAVAILABLE;
     };
 
-    db.delete_item(uuid.to_string());
+    db.delete_item(list_uuid.to_string(), item_uuid.to_string());
 
     StatusCode::OK
 }
--- backend/src/database.rs
+++ backend/src/database.rs
@@ -6,6 +6,40 @@ pub struct ShoppingItem {
     pub creator: String,
 }
 
+struct ShoppingList {
+    list: HashMap<String, ShoppingItem>,
+}
+
+impl ShoppingList {
+    fn iter(&self) -> impl Iterator<Item = (&Uuid, &ShoppingItem)> {
+        self.list.iter()
+    }
+}
+
+impl Default for ShoppingList {
+    fn default() -> Self {
+        Self {
+            list: [
+                (
+                    "6855cfc9-78fd-4b66-8671-f3c90ac2abd8".to_string(),
+                    ShoppingItem {
+                        title: "Coffee".to_string(),
+                        creator: "Roland".to_string(),
+                    },
+                ),
+                (
+                    "3d778d1c-5a4e-400f-885d-10212027382d".to_string(),
+                    ShoppingItem {
+                        title: "Tomato Seeds".to_string(),
+                        creator: "Tania".to_string(),
+                    },
+                ),
+            ]
+            .into(),
+        }
+    }
+}
+
 impl ShoppingItem {
     pub fn to_model(&self, uuid: impl AsRef<str>) -> model::ShoppingListItem {
         model::ShoppingListItem {
@@ -28,45 +62,47 @@ impl From<model::PostShopItem> for ShoppingItem {
 type Uuid = String;
 
 pub struct InMemoryDatabase {
-    inner: HashMap<String, ShoppingItem>,
+    inner: HashMap<String, ShoppingList>,
 }
 impl InMemoryDatabase {
-    fn get_items(&self, uuid: &str) -> Option<&ShoppingItem> {
-        self.inner.get(uuid)
+    pub fn insert_item(
+        &mut self,
+        list_uuid: impl AsRef<str>,
+        item_uuid: impl AsRef<str>,
+        shopping_item: ShoppingItem,
+    ) {
+        self.inner.get_mut(list_uuid.as_ref()).and_then(|list| {
+            list.list
+                .insert(item_uuid.as_ref().to_owned(), shopping_item)
+        });
     }
 
-    pub fn insert_item(&mut self, uuid: impl AsRef<str>, item: ShoppingItem) {
-        self.inner.insert(uuid.as_ref().to_owned(), item);
+    pub fn delete_item(&mut self, list_uuid: impl AsRef<str>, item_uuid: impl AsRef<str>) {
+        self.inner
+            .get_mut(list_uuid.as_ref())
+            .and_then(|list| list.list.remove(item_uuid.as_ref()));
     }
 
-    pub fn delete_item(&mut self, uuid: impl AsRef<str>) {
-        self.inner.remove(uuid.as_ref());
+    pub fn create_list(&mut self, list_uuid: impl AsRef<str>) {
+        self.inner
+            .insert(list_uuid.as_ref().to_owned(), ShoppingList::default());
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = (&Uuid, &ShoppingItem)> {
-        self.inner.iter()
+    pub fn iter_list(
+        &self,
+        list_uuid: impl AsRef<str>,
+    ) -> Option<impl Iterator<Item = (&Uuid, &ShoppingItem)>> {
+        self.inner.get(list_uuid.as_ref()).map(|l| l.iter())
     }
 }
 
 impl Default for InMemoryDatabase {
     fn default() -> Self {
         Self {
-            inner: [
-                (
-                    "c0d35dad-d567-4025-a7c5-e31d793cd60c".to_owned(),
-                    ShoppingItem {
-                        title: "Pizza".to_owned(),
-                        creator: "Douglas".to_string(),
-                    },
-                ),
-                (
-                    "2781043b-ebde-4a45-84e1-c5e5a5a4e6e6".to_owned(),
-                    ShoppingItem {
-                        title: "10x Brocollo".to_owned(),
-                        creator: "Alice".to_owned(),
-                    },
-                ),
-            ]
+            inner: [(
+                "9e137e61-08ac-469d-be9d-6b3324dd20ad".to_string(),
+                ShoppingList::default(),
+            )]
             .into(),
         }
     }
