a2cc9a79b220069d3523cdc8f5f6ede4c72d204c
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-27 15:51:55 +0200

    add post item frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,9 @@
 use dioxus::prelude::*;
-use model::ShoppingListItem;
+use model::{PostShopItem, ShoppingListItem};
 
 const _STYLE: &str = manganis::mg!(file("public/tailwind.css"));
+
+struct ListChanged;
 
 fn main() {
     launch(App);
@@ -9,6 +11,7 @@ fn main() {
 
 #[allow(non_snake_case)]
 pub fn App() -> Element {
+    let change_signal = use_signal(|| ListChanged);
     let rust_basel = "Rust Basel";
     rsx! {
         h1{
@@ -18,13 +21,17 @@ pub fn App() -> Element {
             class: "btn",
             "My stylish button"
         }
-        ShoppingList{}
+        ShoppingList{change_signal}
+        ItemInput{change_signal}
     }
 }
 
 #[component]
-fn ShoppingList() -> Element {
-    let items_request = use_resource(move || async move { get_items().await });
+fn ShoppingList(change_signal: Signal<ListChanged>) -> Element {
+    let items_request = use_resource(move || async move {
+        change_signal.read();
+        get_items().await
+    });
 
     match &*items_request.read_unchecked() {
         Some(Ok(list)) => rsx! {
@@ -82,4 +89,74 @@ async fn get_items() -> Result<Vec<ShoppingListItem>, reqwest::Error> {
         .await?
         .json::<Vec<ShoppingListItem>>()
         .await
+}
+
+async fn post_item(item: PostShopItem) -> Result<ShoppingListItem, reqwest::Error> {
+    reqwest::Client::new()
+        .post("http://localhost:3001/items")
+        .json(&item)
+        .send()
+        .await?
+        .json::<ShoppingListItem>()
+        .await
+}
+
+#[component]
+fn ItemInput(change_signal: Signal<ListChanged>) -> Element {
+    let mut item = use_signal(|| String::new());
+    let mut author = use_signal(|| String::new());
+
+    // We implement this closure later
+    let onsubmit = move |_| {
+        spawn({
+            async move {
+                let item_name = item.read().to_string();
+                let author = author.read().to_string();
+                if let Ok(_) = post_item(PostShopItem {
+                    title: item_name,
+                    posted_by: author,
+                })
+                .await
+                {
+                    change_signal.write();
+                }
+            }
+        });
+    };
+
+    rsx! {
+        div {
+            class: "w-300 m-4 mt-16 rounded",
+            form { class: "grid grid-cols-3 gap-2",
+                onsubmit: onsubmit,
+                div {
+                    input {
+                        value: "{item}",
+                        class: "input input-bordered input-primary w-full",
+                        placeholder: "next item..",
+                        r#type: "text",
+                        id: "item_name",
+                        name: "item_name",
+                        oninput: move |e| item.set(e.data.value().clone())
+                    }
+                }
+                div {
+                    input {
+                        value: "{author}",
+                        class: "input input-bordered input-primary w-full",
+                        placeholder: "wanted by..",
+                        r#type: "text",
+                        id: "author",
+                        name: "author",
+                        oninput: move |e| author.set(e.data.value().clone())
+                    }
+                }
+                button {
+                    class: "btn btn-primary w-full",
+                    r#type: "submit",
+                    "Commit"
+                }
+            }
+        }
+    }
 }
