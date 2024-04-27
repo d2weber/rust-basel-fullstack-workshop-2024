09cd22e51a4480726df191ec79e315cbf177ac15
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-27 11:22:41 +0200

    add first fullstack route

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use model::ShoppingListItem;
 
 fn main() {
     launch(App);
@@ -6,5 +7,77 @@ fn main() {
 
 #[allow(non_snake_case)]
 pub fn App() -> Element {
-    rsx! {"Hello World"}
+    let rust_basel = "Rust Basel";
+    rsx! {
+        h1{
+            "Welcome to {rust_basel}"
+        }
+        button{
+            class: "btn",
+            "My stylish button"
+        }
+        ShoppingList{}
+    }
+}
+
+#[component]
+fn ShoppingList() -> Element {
+    let items_request = use_resource(move || async move { get_items().await });
+
+    match &*items_request.read_unchecked() {
+        Some(Ok(list)) => rsx! {
+            div { class: "grid place-items-center min-h-500",
+                ul {
+                    class: "menu bg-base-200 w-200 rounded-box gap-1",
+                    for i in list {
+                        li {
+                            key: "{i.uuid}",
+                            ShoppingListItemComponent{
+                                display_name: i.title.clone(),
+                                posted_by: i.posted_by.clone()
+                            },
+                        }
+                    }
+                }
+            }
+        },
+        Some(Err(err)) => {
+            rsx! {
+                p {
+                    "Error: {err}"
+                }
+            }
+        }
+        None => {
+            rsx! {
+                p {
+                    "Loading items..."
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn ShoppingListItemComponent(display_name: String, posted_by: String) -> Element {
+    rsx! {
+        div {
+            class: "flex items-center space-x-2",
+            p {
+                class: "grow text-2xl",
+                "{display_name}"
+            }
+            span {
+                "posted by {posted_by}"
+            }
+        }
+    }
+}
+
+async fn get_items() -> Result<Vec<ShoppingListItem>, reqwest::Error> {
+    let url = "http://localhost:3001/items";
+    reqwest::get(url)
+        .await?
+        .json::<Vec<ShoppingListItem>>()
+        .await
 }
